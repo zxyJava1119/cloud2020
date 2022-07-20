032f17f779fe81fc4a4946dcb85a39a47252dd96
Layne <Layne@qq.com>
2022-07-20 16:47:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/springcloud/Payment8001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//服务提供者
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class Payment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/springcloud/Payment8001Tests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Payment8001Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
